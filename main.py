19a1ec34c7f58c800f0f3bd9e60751b88769d0aa
GabrielRenno <gabriel_renno@outlook.com>
2024-11-07 15:04:35 -0600

    commit

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 import os
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Set the OpenAI API key
-os.environ["OPENAI_API_KEY"] = ""
+#os.environ["OPENAI_API_KEY"] = ""
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
