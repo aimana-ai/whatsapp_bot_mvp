cee3bc3283a63a3f47ada217a03a72320b3ec680
GabrielRenno <gabriel_renno@outlook.com>
2024-11-07 15:16:00 -0600

    commit

--- whatsapp_bot/src/whatsapp_bot/crew.py
+++ whatsapp_bot/src/whatsapp_bot/crew.py
@@ -2,46 +2,35 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 
 # Uncomment the following line to use an example of a custom tool
-# from whatsapp_bot.tools.custom_tool import MyCustomTool
+# from whatsapp_crew.tools.custom_tool import MyCustomTool
 
 # Check our tools documentations for more information on how to use them
 # from crewai_tools import SerperDevTool
 
 @CrewBase
-class WhatsappBotCrew():
-	"""WhatsappBot crew"""
+class WhatsappCrewCrew():
+	"""WhatsappCrew crew"""
 
 	@agent
-	def researcher(self) -> Agent:
+	def assistant(self) -> Agent:
 		return Agent(
-			config=self.agents_config['researcher'],
+			config=self.agents_config['assistant'],
 			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
 			verbose=True
 		)
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True
-		)
 
 	@task
-	def research_task(self) -> Task:
+	def assistant_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['research_task'],
+			config=self.tasks_config['assistant_task'],
 		)
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
-		)
+
 
 	@crew
 	def crew(self) -> Crew:
-		"""Creates the WhatsappBot crew"""
+		"""Creates the WhatsappCrew crew"""
 		return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
--- whatsapp_bot/src/whatsapp_bot/main.py
+++ whatsapp_bot/src/whatsapp_bot/main.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 import sys
-from whatsapp_bot.crew import WhatsappBotCrew
+from crew import WhatsappCrewCrew
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+#load_dotenv()
+
+# Set the OpenAI API key
+#os.environ["OPENAI_API_KEY"] = ""
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
 
+# Create a test file with the messages from the last 24 hours
+# and run the crew with it.
+messages_dict = {'message': 'AI LLMs',
+                 'sent_at': '2024-11-07',
+                 'sent_by': 'John Doe',
+                 'contact_name': 'Jane Doe',
+                 'contact_number': 'Jane Doe',
+                 'category': 'work'}
+
 def run():
     """
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs'
+        'messages': messages_dict
     }
-    WhatsappBotCrew().crew().kickoff(inputs=inputs)
+    WhatsappCrewCrew().crew().kickoff(inputs=inputs)
 
 
 def train():
@@ -22,10 +39,10 @@ def train():
     Train the crew for a given number of iterations.
     """
     inputs = {
-        "topic": "AI LLMs"
+        "messages": messages_dict
     }
     try:
-        WhatsappBotCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        WhatsappCrewCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
@@ -35,7 +52,7 @@ def replay():
     Replay the crew execution from a specific task.
     """
     try:
-        WhatsappBotCrew().crew().replay(task_id=sys.argv[1])
+        WhatsappCrewCrew().crew().replay(task_id=sys.argv[1])
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
@@ -45,10 +62,14 @@ def test():
     Test the crew execution and returns the results.
     """
     inputs = {
-        "topic": "AI LLMs"
+        "messages": messages_dict
     }
     try:
-        WhatsappBotCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+        WhatsappCrewCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
+
+
+if __name__ == "__main__":
+    run()
